602e0263231b2c56653db568eecfaf56d65c69dc
Aurelie.Bergey <aurelie.bergey@gmail.com>
2021-12-30 16:17:23 +0100

    ?? NEW: mise a jour

--- exercice1.php
+++ exercice1.php
@@ -1,6 +1,17 @@
 <?php
-// ecrire une chaine de caractère a l'envers dans le terminal
+// ecrire une chaine de caractère a l'envers dans le terminal  de deux façons
 
-echo strrev("Leon a rase Cesar a Noel")
+echo strrev("Leon a rase Cesar a Noel");
+
+$mot = "Engage le jeu que je le gagne ";
+$array = str_split($mot);
+print_r ($array);
+$nb = strlen($mot);
+
+
+for ( $i = $nb -1 ; $i >= 0;$i--){
+    echo($array[$i]);
+}
+      
 
 ?>
